2af9ff64f3b1098a492cbb24773ff533208d36bf
Callum Bennett <callumbennett.cb@gmail.com>
2021-10-09 00:25:20 +0100

    Add product card and grid components, add basic styles

---
+++ components/product/ProductCard/ProductCard.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import { Product } from "@common/types/product";
+import Link from "next/link";
+import Image from "next/image";
+
+interface Props {
+  product: Product;
+}
+
+const placeholderImage = "/product-image-placeholder.csv";
+
+const ProductCard: FC<Props> = ({ product }) => {
+  return (
+    <Link href={`/products/${product.slug}`}>
+      <a className="">
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>£23</span>
+        </div>
+        {product.images && (
+          <Image
+            alt={product.name ?? "Product image"}
+            src={placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
+      </a>
+    </Link>
+  );
+};
+
+export default ProductCard;
