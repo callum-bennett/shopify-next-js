737b625086b8f6095256b05512268ff56bfd3301
Callum Bennett <callumbennett.cb@gmail.com>
2021-10-16 12:38:34 +0100

    Add ui reducer for handling drawer

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, useContext, useState } from "react";
+import { createContext, FC, useContext, useReducer } from "react";
 
 export interface StateModifiers {
   openSidebar: () => void;
@@ -25,14 +25,37 @@ const UIContext = createContext<State>({
   ...initialState,
 });
 
+type Action = {
+  type: "OPEN_SIDEBAR" | "CLOSE_SIDEBAR";
+};
+
+function uiReducer(state: StateValues, action: Action) {
+  switch (action.type) {
+    case "OPEN_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: true,
+      };
+    }
+    case "CLOSE_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: false,
+      };
+    }
+  }
+}
+
 export const UIProvider: FC = ({ children }) => {
-  const openSidebar = () => alert("Opening sidebar");
-  const closeSidebar = () => alert("Closing sidebar");
+  const [state, dispatch] = useReducer(uiReducer, initialState);
+
+  const openSidebar = () => dispatch({ type: "OPEN_SIDEBAR" });
+  const closeSidebar = () => dispatch({ type: "CLOSE_SIDEBAR" });
 
   const uiState = {
+    ...state,
     openSidebar,
     closeSidebar,
-    isSidebarOpen: false,
   };
 
   return <UIContext.Provider value={uiState}>{children}</UIContext.Provider>;
