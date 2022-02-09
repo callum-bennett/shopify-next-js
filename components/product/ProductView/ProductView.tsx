5cdb66d7f6a3d5cb3ff849517c07d48441e7318c
Callum Bennett <callumbennett.cb@gmail.com>
2022-02-09 23:25:43 +0000

    Add custom UI button to product view

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,7 +1,7 @@
 import cn from "classnames";
 import { FC } from "react";
 import s from "./ProductView.module.css";
-import { Container } from "@components/ui";
+import { Button, Container } from "@components/ui";
 import Image from "next/image";
 import { Product } from "@common/types/product";
 import { ProductSlider } from "@components/product";
@@ -49,13 +49,7 @@ const ProductView: FC<Props> = ({ product }) => {
             </div>
           </section>
           <div>
-            <button
-              onClick={() => {}}
-              aria-label="Add to Cart"
-              className={s.button}
-            >
-              Add to Cart
-            </button>
+            <Button className={s.button} onClick={() => alert("adding to cart")}>Add to Cart</Button>
           </div>
         </div>
       </div>
