3c1c96c74b5cdd65d0eeb8af2c4950c5cd001238
Callum Bennett <callumbennett.cb@gmail.com>
2022-02-09 23:13:28 +0000

    Add keen slider to Product view

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,10 +1,37 @@
-import { FC } from "react";
+import { FC, cloneElement, Children, isValidElement } from "react";
+import { useKeenSlider } from "keen-slider/react";
 import s from "./ProductSlider.module.css";
+import cn from "classnames";
 
 const ProductSlider: FC = ({ children }) => {
+  const [sliderRef, slider] = useKeenSlider({
+    initial: 0,
+    loop: true,
+  });
+
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity">{children}</div>
+      <div ref={sliderRef} className="keen-slider h-full transition-opacity">
+        <button
+          onClick={() => slider.current?.prev()}
+          className={cn(s.leftControl, s.control)}
+        />
+        {Children.map(children, (child) => {
+          if (isValidElement(child)) {
+            return cloneElement(child, {
+              className: `${
+                child.props.className ? `${child.props.className}` : ""
+              } keen-slider__slide`,
+            });
+          }
+
+          return child;
+        })}
+        <button
+          onClick={() => slider.current?.next()}
+          className={cn(s.rightControl, s.control)}
+        />
+      </div>
     </div>
   );
 };
